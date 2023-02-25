cc509cb9b5bc9bf525ac22307e094ab500c29a82
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-25 12:44:33 +0100

    css update

--- resources/views/includes/movies/card.blade.php
+++ resources/views/includes/movies/card.blade.php
@@ -1,6 +1,6 @@
 @if (isset($movie))
 <div class="col-12 col-md-6 col-lg-3">
-    <div class="my-card p-3 text-center text-white">
+    <div class="my-card p-3 text-center">
         <figure><img class="img-fluid" src="{{ $movie->poster }}" alt=""></figure>
         <h5>{{ $movie->title }}</h5>
         <h5>{{ $movie->nationality }}</h5>
