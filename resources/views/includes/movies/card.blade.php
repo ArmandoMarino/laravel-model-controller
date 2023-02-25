d9276416838a82ffa4ae6c4a1246b0e78b095777
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-25 12:28:34 +0100

    some css

--- resources/views/includes/movies/card.blade.php
+++ resources/views/includes/movies/card.blade.php
@@ -1,8 +1,10 @@
 @if (isset($movie))
-<div class="my-card p-3 text-center">
-    <figure><img class="img-fluid" src="" alt=""></figure>
-    <h5>{{ $movie->title }}</h5>
-    <p></p>
-    <h5></h5>
-</div>
+<div class="col-12 col-md-6 col-lg-3">
+    <div class="my-card p-3 text-center text-white">
+        <figure><img class="img-fluid" src="{{ $movie->poster }}" alt=""></figure>
+        <h5>{{ $movie->title }}</h5>
+        <h5>{{ $movie->nationality }}</h5>
+        <p>Anno : {{ $movie->date }}</p>
+    </div>
+</div>   
 @endif
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -5,12 +5,14 @@
 
 {{-- HTML --}}
 @section('content')
-<div id="content-home">
-    @forelse ($movies as $movie)
-        @include('includes.movies.card')
-        @empty
-        <h2>Nessun risultato</h2>
-    @endforelse
+<div id="content-movies" class="container my-5">
+    <div class="row g-4">
+        @forelse ($movies as $movie)
+            @include('includes.movies.card')
+            @empty
+            <h2>Nessun risultato</h2>
+        @endforelse
+    </div>   
 </div>
 @endsection
 
